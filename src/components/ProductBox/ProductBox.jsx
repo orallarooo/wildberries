94374459680c384e9803f3453d1176c0ca16db70
orallarooo <kdirbaevoral875@gmail.com>
2024-09-06 20:12:49 +0500

    06

---
+++ src/components/ProductBox/ProductBox.jsx
@@ -0,0 +1,51 @@
+import "./ProductBox.css"
+
+import star_img from "./img/star.svg"
+import verified_img from "./img/verify.svg"
+import product_img from "./img/1.webp"
+import product_img_second from "./img/2.webp"
+import basket_img from "./img/basket.svg"
+
+
+const ProductBox = () => {
+      return ( 
+            <a href="#" className="product-box">
+                  <div className="product-box__img">
+                        <img src={product_img_second} alt="" />
+                        <a href="#" className="product-box__img-link">
+                              <p>
+                                Быстрый просмотр    
+                              </p>
+                        </a>
+                  </div>
+                  <div className="product-bottom">
+                        <p className="product-box__price">
+                              от   3 156 ₽
+                        </p>
+                        <p className="product-box__basket"> 
+                              с WB кошельком
+                        </p>
+                        <p className="product-box__firm">
+                              <img src={verified_img} alt="" />
+                              UZcotton 
+                              <span>  
+                                    / Футболка черная одежда
+                              </span>
+                        </p>
+                        <p className="product-box__assessment">
+                              <img src={star_img} alt="" />
+                              4.9 
+                              <span>
+                                    •  264 748 оценок
+                              </span>
+                        </p>
+                        <a href="#" className="product-box__btn">
+                              <img src={basket_img} alt="" />
+                              14 сентября
+                        </a>
+                  </div>
+            </a>
+       );
+}
+ 
+export default ProductBox;
---
+++ src/components/ProductBoxes/ProductBoxes.jsx
@@ -0,0 +1,81 @@
+import "./ProductBoxes.css"
+
+import ProductBox from "../ProductBox/ProductBox";
+
+
+const ProductBoxes = () => {
+      return ( 
+            <div className="product-boxes">
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+                  <ProductBox />
+            </div>
+      );
+}
+ 
+export default ProductBoxes;
+
+
