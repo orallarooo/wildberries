1c93154395db1855ffe14b9133346d9676714a9d
orallarooo <kdirbaevoral875@gmail.com>
2024-09-15 00:43:54 +0500

    15-09

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './css/App.css';
 import Header from './components/Header/Header';
 import ProductBoxes from './components/ProductBoxes/ProductBoxes';
 import BottomMenu from './components/BottomMenu/BottomMenu';
+import SimpleSlider from './components/Carousel/Carousel';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SimpleSlider />
       <ProductBoxes/>
       <BottomMenu />
     </div>
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "./Carousel.css"
+
+import slider_img from "./img/uhnydazha_2880_1.webp"
+import slider_img_2 from "./img/del_2880_uz.webp"
+
+
+function SampleNextArrow(props) {
+      const { className, style, onClick } = props;
+            return (
+            <div
+                  className={className}
+                  style={{ ...style, display: "block", background: "red" }}
+                  onClick={onClick}
+            />
+            );
+      }
+    
+      function SamplePrevArrow(props) {
+            const { className, style, onClick } = props;
+            return (
+            <div
+                  className={className}
+                  style={{ ...style, display: "block", background: "green" }}
+                  onClick={onClick}
+            />
+            );
+      }
+
+export default function SimpleSlider() {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+      <div className="slider-container">
+            <Slider {...settings} className="slider">
+                  <div className="slider__box" >
+                  </div>
+                  <div className="slider__box">
+                        <img src={slider_img_2} alt="" />
+                  </div>
+                  <div className="slider__box">
+                        <img src={slider_img} alt="" />
+                  </div>
+                  <div className="slider__box">
+                        <img src={slider_img_2} alt="" />
+                  </div>
+            </Slider>
+    </div>
+  );
+}
