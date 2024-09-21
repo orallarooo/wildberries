d459827485f873ee5cd80f6fd9184f5790134fa2
orallarooo <kdirbaevoral875@gmail.com>
2024-09-21 20:10:17 +0500

    20-final-2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react";
 
-
-import './css/App.css';
-import Header from './components/Header/Header';
-import ProductBoxes from './components/ProductBoxes/ProductBoxes';
-import SimpleSlider from './components/Carousel/Carousel';
-
+import "./css/App.css";
+import Header from "./components/Header/Header";
+import ProductBoxes from "./components/ProductBoxes/ProductBoxes";
+// import SimpleSlider from './components/Carousel/Carousel';
 
 function App() {
+  const Footer = lazy(() => import("./components/Footer/Footer"));
+  const SimpleSlider = lazy(() => import("./components/Carousel/Carousel"));
 
-  const Footer = lazy(() => import('./components/Footer/Footer'));
-  
   return (
     <div className="App">
       <Header />
-      <SimpleSlider />
-      <ProductBoxes/>
+
+      <Suspense
+        fallback={
+          <div
+            style={{
+              height: "168px",
+              margin: "28px 0",
+            }}
+          ></div>
+        }
+      >
+        <SimpleSlider />
+      </Suspense>
+
+      <ProductBoxes />
 
       <Suspense fallback={<div>Loading...</div>}>
         <Footer />
